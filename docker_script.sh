0b24e757d2982c064fd15208b861e3b0991c991e
tenkgrow <32664219+tenkgrow@users.noreply.github.com>
2018-05-07 10:34:47 +0200

    Update docker_script.sh

--- docker_script.sh
+++ docker_script.sh
@@ -1,23 +1,25 @@
 # Pre-updating
 sudo apt-get update
 # Using repositories over HTTPS
-sudo apt-get\
-install apt-transport-https \
-install ca-certificates \
--y install curl \
-install software-properties-common
+sudo apt-get install\
+apt-transport-https \
+ca-certificates \
+install curl \
+software-properties-common
 # Add Docker's official GPG key
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 # Fingerprint verification
 sudo apt-key fingerprint 0EBFCD88
 # Setting up a stable repository
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo add-apt-repository \
+"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+$(lsb_release -cs) \
+stable"
 # Install Docker CE
-sudo apt-get \
--y update \
-install docker-ce
+sudo apt-get update
+sudo apt-get install docker-ce
 # Updating to latest version (specific version: '=<version>')
-apt-cache madison docker-ce
+sudo apt-cache madison docker-ce
 # Checking for functionality and version
 sudo docker run hello-world
 sudo docker --version
